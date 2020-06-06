afde0a7d1f67c39aeba28d32172820b9c907ba3d
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2020-06-06 03:28:25 -0700

    Add is_image extension validator

--- src/validators.cpp
+++ src/validators.cpp
@@ -1,9 +1,14 @@
 #include "validators.hpp"
 
+#include <algorithm>
 #include <filesystem>
 #include <stdexcept>
+#include <string>
+#include <vector>
 
 namespace fs = std::filesystem;
+
+static std::vector< std::string > image_extensions = {".png", ".jpg", ".jpeg", ".tiff", ".gif"};
 
 void ensure_path_is_a_folder( std::string const &path_str )
 {
@@ -15,4 +20,12 @@ void ensure_path_is_a_folder( std::string const &path_str )
                                  "  Invalid path: [" +
                                  path_str + "]" );
     }
+}
+
+bool is_image( std::filesystem::path const &file_path )
+{
+    return std::any_of(
+        image_extensions.begin(), image_extensions.end(), [&file_path]( auto const &extension ) {
+            return extension == file_path.extension().string();
+        } );
 }
--- src/validators.hpp
+++ src/validators.hpp
@@ -1,8 +1,10 @@
 #ifndef VALIDATORS_HPP
 #define VALIDATORS_HPP
 
+#include <filesystem>
 #include <string>
 
 void ensure_path_is_a_folder( std::string const &path_str );
+bool is_image( std::filesystem::path const &file_path );
 
 #endif
