a264c2290d9e686c9599951fa2316a3dc9aca0bf
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2020-06-06 02:44:22 -0700

    Add get_file_list

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,26 @@
+#include <filesystem>
+#include <string>
+#include <vector>
+
+namespace fs = std::filesystem;
+
+std::vector< std::filesystem::directory_entry > get_file_list( std::string const &folder_path );
+
 int main()
 {
 }
 
+std::vector< std::filesystem::directory_entry > get_file_list( std::string const &folder_path )
+{
+    std::vector< std::filesystem::directory_entry > files;
+
+    for( auto &file : fs::directory_iterator( folder_path ) )
+    {
+        if( file.is_regular_file() )
+        {
+            files.emplace_back( file );
+        }
+    }
+
+    return files;
+}
